3ee5126b103c9bd3c900d4dc237d5db9d1e84357
Chongyi <xpz3847878@163.com>
2022-01-18 11:58:35 +0800

    撤回修改

--- src/service.rs
+++ src/service.rs
@@ -75,7 +75,7 @@ where
 {
     type Rejection = Error;
 
-    async fn from_request(req: &mut RequestParts<B>) -> std::result::Result<Self, Self::Rejection> {
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
         log::debug!("Extract Service extension.");
         let res = req
             .extensions()
